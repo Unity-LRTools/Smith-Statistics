78b1c802256467d13c9108e28870eefb9940de54
Unity-LRTools <pro.lucasrossignol@gmail.com>
2024-11-21 16:26:43 +0100

    Create a ListStatistic class to implement method specific to statistic

---
+++ Runtime/ListStatistic.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LRT.Smith.Statistics
+{
+	public class ListStatistic : List<Statistic>
+    {
+        /// <summary>
+        /// Return the sum of the desired type
+        /// </summary>
+        /// <param name="type">The desired statistic tpe</param>
+        /// <returns>A sum</returns>
+        public float Sum(string type)
+        {
+            return this.Where(s => s.Type == type).Sum(s => s.Value);
+        }
+
+        /// <summary>
+        /// Count the total number of different statistic in the list
+        /// </summary>
+        /// <returns>The count of different statistic</returns>
+        public int CountStatistic()
+        {
+            return this.GroupBy(s => s.Type).Count();
+        }
+    }
+}
